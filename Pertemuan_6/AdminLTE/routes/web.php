38cfbca71fdb489c3ad10a13c8ec4be3978c41b3
KiiiRifki_14 <muhammadrifki08t@gmail.com>
2026-04-08 08:31:45 +0700

    refactor: remove unused configuration file

--- Pertemuan_6/AdminLTE/routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('home-lte', 'App\Http\Controllers\HomeController@homeLte');
-Route::get('my-users', 'App\Http\Controllers\HomeController@myUsers');
